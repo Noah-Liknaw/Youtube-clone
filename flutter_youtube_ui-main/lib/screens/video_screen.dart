f41e3a0a42a69a67e3d8d6fe8b4caa68a23de27f
Noah-Liknaw <noahliknawnew@gmail.com>
2023-08-11 23:46:39 +0300

    added streaming functionality

---
+++ flutter_youtube_ui-main/lib/screens/video_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_youtube_ui/data.dart';
+import 'package:flutter_youtube_ui/screens/nav_screen.dart';
+import 'package:flutter_youtube_ui/widgets/widgets.dart';
+import 'package:miniplayer/miniplayer.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class VideoScreen extends StatefulWidget {
+  @override
+  _VideoScreenState createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+
+
+  }
+
+  @override
+  void dispose() {
+    _scrollController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => context
+          .read(miniPlayerControllerProvider)
+          .state
+          .animateToHeight(state: PanelState.MAX),
+      child: Scaffold(
+        body: Container(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          child: CustomScrollView(
+            controller: _scrollController,
+            shrinkWrap: true,
+            slivers: [
+              SliverToBoxAdapter(
+                child: Consumer(
+                  builder: (context, watch, _) {
+                    final selectedVideo = watch(selectedVideoProvider).state;
+                    late YoutubePlayerController _controller;
+                        final videoID = YoutubePlayer.convertUrlToId('https://www.youtube.com/watch?v=umhl2hakkcY');
+                    _controller = YoutubePlayerController(initialVideoId: videoID!, flags: const YoutubePlayerFlags(autoPlay: true));
+                    return SafeArea(
+                      child: Column(
+                        children: [
+                          Stack(
+                            children: [
+                              YoutubePlayer(controller: _controller,
+                              showVideoProgressIndicator: true,),
+                              
+                              IconButton(
+                                iconSize: 30.0,
+                                icon: const Icon(Icons.keyboard_arrow_down),
+                                onPressed: () => context
+                                    .read(miniPlayerControllerProvider)
+                                    .state
+                                    .animateToHeight(state: PanelState.MIN),
+                              ),
+                            ],
+                          ),
+                          const LinearProgressIndicator(
+                            value: 0.4,
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              Colors.red,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    final video = suggestedVideos[index];
+                    return VideoCard(
+                      video: video,
+                      hasPadding: true,
+                      onTap: () => _scrollController!.animateTo(
+                        0,
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.easeIn,
+                      ),
+                    );
+                  },
+                  childCount: suggestedVideos.length,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
